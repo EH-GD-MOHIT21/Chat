2d73d79b402a17165885bf63678a2b1539970794
Mohit Satija <70685094+EH-GD-MOHIT21@users.noreply.github.com>
2021-10-03 12:35:02 +0530

    Update asgi.py

--- manager/asgi.py
+++ manager/asgi.py
@@ -1,27 +1,19 @@
-"""
-ASGI config for manager project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
 from django.conf.urls import url
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manager.settings')
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack   
 import chatroom.routing
 
 
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "http": django_asgi_app,
+    'websocket': AuthMiddlewareStack(
         URLRouter(
             chatroom.routing.websocket_urlpatterns
         )
