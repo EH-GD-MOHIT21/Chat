349436b38d4e9f6718887b76026fad338ee4af39
Mohit Satija <70685094+EH-GD-MOHIT21@users.noreply.github.com>
2021-10-03 15:07:28 +0530

    Update settings.py

--- manager/settings.py
+++ manager/settings.py
@@ -148,10 +148,7 @@ OTHER_STATICS = os.path.join(BASE_DIR,'public')
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
     },
 }
 
